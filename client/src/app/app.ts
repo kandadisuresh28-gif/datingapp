39f2c24dd00142567e8318ae75d78a8feb35b4d2
kandadisuresh28-gif <kandadi.suresh28@gmail.com>
2026-03-21 14:00:52 +0530

    End Session 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+
+  private http  = inject(HttpClient)
+  protected  title = 'Dating App';
+  protected members =signal<any>([]);
+  protected t1 ="Testing";
+
+
+  async ngOnInit() {
+    
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+
+    try {
+
+      return lastValueFrom (this.http.get('https://localhost:7023/api/members'));
+      
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+  }
+
+
+  //constructor(private http: HttpClient) {}
+}
